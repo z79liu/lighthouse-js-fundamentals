3ce24422b630001a38415e7e2de7ac620534f64e
Zeyu Liu <haozai_03@hotmail.com>
2019-02-02 01:29:32 +0000

    conditional statement pratice

---
+++ go-outside.js
@@ -0,0 +1,15 @@
+var raining = true;
+
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+
+var cold = false;
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!");
